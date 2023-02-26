3a8038293e29b58246f10270c2c0665c7fe24e41
makaka <75128685+makk521@users.noreply.github.com>
2023-02-26 23:02:11 +0800

    update

---
+++ read_data.js
@@ -0,0 +1,20 @@
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('C:/Users/ASUS/Desktop/iot_keshe/data.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
+
+
+
+let sql = `SELECT * FROM data `;
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
